231724fe6e057d56b84d3e206f94a8b35fc73651
Marissa D'Antonio <dantonma@bc.edu>
2020-12-11 11:17:26 -0500

    Initial Commit

---
+++ Student Planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Planner
+//
+//  Created by Marissa D'Antonio on 12/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
